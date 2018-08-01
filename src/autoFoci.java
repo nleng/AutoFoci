be243f2e5bb2a073a1a326909ce50ce395bf393c
Nicor Lengert <nicorlengert@gmx.de>
2018-08-01 11:19:34 +0200

    Add files via upload

---
+++ src/autoFoci.java
@@ -0,0 +1,10 @@
+
+import autoFoci.MainFrame;
+ 
+public class autoFoci {
+    public static void main(String[] args) {
+
+        MainFrame dialog = new MainFrame();
+        dialog.create_tabbed_dialog();
+    }
+}
